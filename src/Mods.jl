b87972ed06d8a5263ccc19cccc17e7608ccc62e0
Ed Scheinerman <ers@jhu.edu>
2020-07-13 16:29:14 -0400

    New Mod structure

--- src/Mods.jl
+++ src/Mods.jl
@@ -1,97 +1,106 @@
 module Mods
 
-import Base.isequal, Base.==, Base.+, Base.-, Base.*
-import Base.inv, Base./, Base.^
-import Base.hash, Base.show
 
-export Mod
-export isequal, ==, +, -, *
-export is_invertible, inv, /, ^
+import Base: isequal, (==), (+), (-), (*), (inv), (/), (^), hash, show
+
+export Mod, modulus, value
+export isequal, ==, +, -, *, is_invertible, inv, /, ^
 export hash, CRT
 
 """
-`Mod(v,m)` creates a modular number in mod `m` with value `v%m`.
-`Mod(m)` is equivalent to `Mod(0,m)`.
+`Mod{m}(v)` (and also `Mod(v,m)`) creates a modular number in mod `m` with value `v%m`.
+`Mod{m}()` is equivalent to `Mod(0,m)`.
 """
-struct Mod{T<:Integer}
-    val::T
-    mod::T
-    function Mod(a::S, m::T) where {S<:Integer, T<:Integer}
-        if m < 1
-            error("Modulus must be at least 1")
-        end
-
-        a,m = promote(a,m)
-        return new{typeof(a)}(mod(a,m),m)
+struct Mod{N}
+    val::Int
+    function Mod(x::Int,N::Int)
+        @assert N>1 ""
+        new{N::Int}(mod(x,N))
     end
 end
 
-Mod(m::Integer) = Mod(0,m)
+Mod{N}(x::Int=0) where N = Mod(x,N)
+
+
+modulus(a::Mod{N}) where N = N
+value(a::Mod{N}) where N = a.val
+
 
 function hash(x::Mod, h::UInt64= UInt64(0))
-    v = BigInt(x.val)
-    m = BigInt(x.mod)
+    v = BigInt(value(x))
+    m = BigInt(modulus(x))
     return hash(v,hash(m,h))
 end
 
 # Test for equality
-isequal(x::Mod, y::Mod) = x.mod==y.mod && x.val==y.val
+isequal(x::Mod, y::Mod) = value(x)==value(y) && modulus(x)==modulus(y)
+
 ==(x::Mod,y::Mod) = isequal(x,y)
 
-function modcheck(x::Mod, y::Mod)
-    if x.mod != y.mod
+
+"""
+`modcheck(x::Mod,y::Mod)` checks if `x` and `y` have the same modulus.
+If so, return than modulus. If not, throw an error.
+"""
+function modcheck(x::Mod, y::Mod)::Int
+    mx = modulus(x)
+    my = modulus(y)
+    if mx != my
         error("Cannot operate on two Mod objects with different moduli")
     end
-    true
+    return mx
 end
 
 # Easy arithmetic
-function +(x::Mod, y::Mod)
-    modcheck(x,y)
+function +(x::Mod, y::Mod)::Mod
+    m = modcheck(x,y)
     s,flag = Base.add_with_overflow(x.val,y.val)
     if !flag
-        return Mod(x.val+y.val, x.mod)
+        return Mod(x.val+y.val, m)
     end
     s = widen(x.val) + widen(y.val)    # add with added precision
-    s = mod(s,x.mod)                   # reduce by modulus
-    return Mod(oftype(x.mod,s),x.mod)  # return with proper type
+    s = Int(mod(s,m))                  # reduce by modulus
+    return Mod(s,m)
 end
 
 
-function -(x::Mod)
-    return Mod(-x.val, x.mod)
+function -(x::Mod{M}) where M
+    return Mod(-x.val, M)
 end
 
 -(x::Mod,y::Mod) = x + (-y)
 
 
 function *(x::Mod, y::Mod)
-    modcheck(x,y)
+    m = modcheck(x,y)
     p,flag = Base.mul_with_overflow(x.val,y.val)
     if !flag
-        return Mod(x.val*y.val, x.mod)
+        return Mod(x.val*y.val, m)
     end
     p = widemul(x.val, y.val)         # multipy with added precision
-    p = mod(p,x.mod)                  # reduce by the modulus
-    return Mod(oftype(x.mod,p),x.mod) # return with proper type
+    p = Int(mod(p,m))                  # reduce by the modulus
+    return Mod(p,m) # return with proper type
 end
 
 # Division stuff
 """
 `is_invertible(x::Mod)` determines if `x` is invertible.
 """
-is_invertible(x::Mod) = return gcd(x.val,x.mod)==1 && x.mod>1
+function is_invertible(x::Mod{M})::Bool where M
+    return gcd(value(x),M) == 1
+end
+
 
 """
 `inv(x::Mod)` gives the multiplicative inverse of `x`.
 This may be abbreviated by `x'`.
 """
-function inv(x::Mod)
-    (g, v, ignore) = gcdx(x.val, x.mod)
-    if g != 1 || x.mod==1
-        error(x, " is not invertible")
+function inv(x::Mod{M}) where M
+    (g, v, ignore) = gcdx(x.val, M)
+    if g != 1
+        @error "$x is not invertible"
     end
-    return Mod(v, x.mod)
+    return Mod(v, M)
 end
 
 # Typing shortcut for inv(x)
@@ -102,13 +111,12 @@ function /(x::Mod, y::Mod)
     return x * inv(y)
 end
 
-function ^(x::Mod, k::Integer)
+function ^(x::Mod{M}, k::Integer) where M
     if k>0
-        return Mod(powermod(x.val, k, x.mod), x.mod)
+        return Mod(powermod(x.val, k, M), M)
     end
     if k==0
-        T = typeof(x.val)
-        return Mod(one(T), x.mod)
+        return Mod(1,M)
     end
     y = inv(x)
     return y^(-k)
@@ -116,21 +124,21 @@ end
 
 # Operations with Integers
 
-+(x::Mod, k::Integer) = Mod(k,x.mod)+x
++(x::Mod{M}, k::Integer) where M = Mod(k,M)+x
 +(k::Integer, x::Mod) = x+k
 
 -(x::Mod, k::Integer) = x + (-k)
 -(k::Integer, x::Mod) = (-x) + k
 
-*(x::Mod, k::Integer) = Mod(k,x.mod) * x
+*(x::Mod{M}, k::Integer) where M = Mod(k,M) * x
 *(k::Integer, x::Mod) = x*k
 
-/(x::Mod, k::Integer) = x / Mod(k, x.mod)
-/(k::Integer, x::Mod) = Mod(k, x.mod) / x
+/(x::Mod{M}, k::Integer) where M = x / Mod(k, M)
+/(k::Integer, x::Mod{M}) where M = Mod(k, M) / x
 
 # Comparison with Integers
 
-isequal(x::Mod, k::Integer) = mod(k,x.mod) == x.val
+isequal(x::Mod{M}, k::Integer) where M = mod(k,M) == x.val
 isequal(k::Integer, x::Mod) = isequal(x,k)
 ==(x::Mod, k::Integer) = isequal(x,k)
 ==(k::Integer, x::Mod) = isequal(x,k)
@@ -138,9 +146,9 @@ isequal(k::Integer, x::Mod) = isequal(x,k)
 # Chinese remainder theorem functions
 
 # private helper function
-function CRT_work(x::Mod, y::Mod)
-    n = x.mod
-    m = y.mod
+function CRT_work(x::Mod{n}, y::Mod{m}) where {n,m}
+    # n = x.mod
+    # m = y.mod
     if gcd(n,m) != 1
         error("Moduli must be pairwise relatively prime")
     end
@@ -172,7 +180,7 @@ julia> 92%14
 function CRT(mtuple::Mod...)
     n = length(mtuple)
     if n == 0
-        return Mod(1)
+        return 1
     end
 
     result::Mod = mtuple[1]
@@ -185,6 +193,6 @@ function CRT(mtuple::Mod...)
 end
 
 
-show(io::IO, m::Mod) = print(io,"Mod(",m.val,",",m.mod,")")
+# show(io::IO, m::Mod{N}) where N = print(io,"Mod(",m.val,",",m.mod,")")
 
 end # end of module Mods
