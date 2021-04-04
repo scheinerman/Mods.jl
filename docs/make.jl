9d2ce688d6a162c0f7cfe072d8b22f256963398d
Charles Kawczynski <kawczynski.charles@gmail.com>
2021-04-04 11:53:27 -0700

    Add start of julia docs

---
+++ docs/make.jl
@@ -0,0 +1,28 @@
+using Documenter, Mods
+
+format = Documenter.HTML(
+    prettyurls = !isempty(get(ENV, "CI", "")),
+    collapselevel = 1,
+)
+
+makedocs(
+    sitename = "Mods.jl",
+    strict = true,
+    format = format,
+    checkdocs = :exports,
+    clean = true,
+    doctest = true,
+    modules = [Mods],
+    pages = Any[
+        "Home" => "index.md",
+        "API" => "api.md",
+    ],
+)
+
+deploydocs(
+    repo = "github.com/scheinerman/Mods.jl.git",
+    target = "build",
+    push_preview = true,
+    devbranch = "main",
+    forcepush = true,
+)
