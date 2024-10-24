51d7d3f5ab86f83f5617a62517b04fe4dd92f083
Ed Scheinerman <ers@jhu.edu>
2024-10-24 09:36:49 -0400

    Bug fix

--- src/basic_functions.jl
+++ src/basic_functions.jl
@@ -24,5 +24,5 @@ iszero(x::AbstractMod) = iszero(value(x))
 isone(x::AbstractMod) = isone(value(x))
 (==)(x::AbstractMod, y::AbstractMod) = modulus(x) == modulus(y) && value(x) == value(y)
 (==)(x::AbstractMod, y::T) where {T<:CZQ} = x == Mod{modulus(x)}(y)
-(==)(x::T, y::AbstractMod) where {T<:CZQ} = Mod{modulus(x)}(x) == y
+(==)(x::T, y::AbstractMod) where {T<:CZQ} = y==x
 
